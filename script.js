db87b721d373057ca68f7a3f5db7915aa8063fa5
MONISHKA SINGH <singhmonishkass@gmail.com>
2022-07-20 02:10:40 +0530

    second commit

--- script.js
+++ script.js
@@ -1,56 +1,114 @@
-var todoInput = document.querySelector(".todo-input");
-var btn = document.querySelector(".todo-button");
-var todoList = document.querySelector(".todo-list");
-
-// Event Handlers
-btn.onclick = create;
-todoList.onclick = performAction;
-
-var data;
-// Functions
-function create(e) {
-  e.preventDefault();
-  data = todoInput.value;
-  console.log(data);
-  data = data.trim();
-  if (data != "") {
-    var newDiv = document.createElement("div");
-    newDiv.classList.add("todo");
-
-    var newLi = document.createElement("li");
-    newLi.classList.add("todo-item");
-    newLi.innerText = data;
-    newDiv.appendChild(newLi);
-
-    var cmpltBtn = document.createElement("button");
-    cmpltBtn.classList.add("cmpltBtn");
-    cmpltBtn.innerHTML = '<i class="fa fa-check " aria-hidden="true"></i>';
-    newDiv.appendChild(cmpltBtn);
-
-    var deleteBtn = document.createElement("button");
-    deleteBtn.classList.add("deleteBtn");
-    deleteBtn.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
-    newDiv.appendChild(deleteBtn);
-
-    todoList.appendChild(newDiv);
-    todoInput.value = "";
-  } else {
-    alert("Box can not be blank");
-  }
+var btn=document.getElementById("btn1");
+btn.onclick=add;
+var arr=[];
+window.onload=fetch;
+function fetch()
+{
+    arr=[];
+    if(!localStorage.getItem("list")==""){
+        arr=localStorage.getItem("list");
+        arr=arr.split(",")
+    }
+    display(arr);
+}
+function display()
+{
+    arr.forEach(element => {
+    let v=element;
+    let item=document.createElement("li");
+    document.getElementById("list").appendChild(item);
+    let d=document.createElement("div");
+    item.appendChild(d);
+    d.className="task";
+    let input=document.createElement("input");
+    input.type="text";
+    input.value=v;
+    input.className="todo list";
+    let i1=document.createElement("div");
+    let i2=document.createElement("div");
+    i1.className="btn tick";
+    i2.className="btn del";
+    i1.innerHTML=`<i class="fa-solid fa-check"></i>`;
+    i2.innerHTML=`<i class="fa-solid fa-trash-can"></i>`;
+    d.appendChild(input);
+    d.appendChild(i1);
+    d.appendChild(i2);
+    i1.onclick=check;
+    i2.onclick=del;
+    });
+}
+function add()
+{
+    let v=document.getElementById("todo").value;
+    v=v.trim();
+    if(!v==""){
+    let item=document.createElement("li");
+    document.getElementById("list").appendChild(item);
+    let d=document.createElement("div");
+    item.appendChild(d);
+    d.className="task";
+    let input=document.createElement("input");
+    input.type="text";
+    input.value=v;
+    input.className="todo list";
+    let i1=document.createElement("div");
+    let i2=document.createElement("div");
+    i1.className="btn tick";
+    i2.className="btn del";
+    i1.innerHTML=`<i class="fa-solid fa-check"></i>`;
+    i2.innerHTML=`<i class="fa-solid fa-trash-can"></i>`;
+    d.appendChild(input);
+    d.appendChild(i1);
+    d.appendChild(i2);
+    i1.onclick=check;
+    i2.onclick=del;
+    arr.push(v);
+    localStorage.setItem("list",arr);
+    document.getElementById("todo").value="";
+    }else alert("To Do cannot be blank");
+}
+function check(e){
+    var i=e.target;
+    var p=i.parentElement;
+    p.classList.toggle("check");
+}
+function del(e){
+    var i=e.target;
+    var p=i.parentElement;
+    var v=p.children[0];
+    p.parentElement.remove(v);
+    arr.pop(v);
+    localStorage.setItem("list",arr);
 }
 
-function performAction(e) {
-  var item = e.target;
-  // console.log(item);
 
-  if (item.classList[0] == "cmpltBtn") {
-    // console.log("Cmplete Button pressed");
-    var parent = item.parentElement;
-    parent.classList.toggle("todo-done");
-  }
-  if (item.classList[0] == "deleteBtn") {
-    var parent = item.parentElement;
-    // console.log(parent);
-    parent.remove();
-  }
-}
+
+time()
+        function time(){
+        document.getElementById("time").innerText=new Date().toLocaleTimeString();
+        setTimeout(time,1000);
+        }
+        var h=new Date().getHours();
+        if(h>=4&&h<12){
+            document.getElementById("clock").innerText="Good Morning";
+        }
+        else if(h>=12&&h<16){
+            document.getElementById("clock").innerText="Good Afternoon";
+        }
+        else if(h>=16&&h<20){
+            document.getElementById("clock").innerText="Good Evening";
+        }
+        else if(h>=20||h<4){
+            document.getElementById("clock").innerText="Good night";
+        }
+        document.getElementById("date").innerText=new Date().toDateString();
+        var n=document.getElementById("name");
+        if(localStorage.getItem("name")==null)
+        {
+           n.value="Enter Name"; 
+        }
+        else n.value=localStorage.getItem("name");
+        function set()
+        {
+            localStorage.setItem("name",n.value);
+        }
